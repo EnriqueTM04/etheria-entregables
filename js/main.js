5b5f4cccc27485d890772556c30fed1cef24a1bb
Manuel32Aguirre <programadorjrsemad@gmail.com>
2024-11-09 00:51:47 -0600

    Nuevo commit, nuevo diseño de la interfaz

--- js/main.js
+++ js/main.js
@@ -9,3 +9,18 @@
     });
   });
 
+  document.addEventListener("DOMContentLoaded", function () {
+    const fadeElements = document.querySelectorAll('.fade-right, .fade-left');
+    const options = {
+        threshold: 0.5
+    };
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+            }
+        });
+    }, options);
+
+    fadeElements.forEach(element => observer.observe(element));
+});
