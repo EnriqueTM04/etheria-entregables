8d6eaf33b23449cc961dfd7b0663b8d8da85ff9d
AxelGasca22 <axel18.gasca08@gmail.com>
2025-01-12 21:10:32 -0600

    "correcciones gasca y sprint 2"

--- js/main.js
+++ js/main.js
@@ -24,3 +24,19 @@
 
     fadeElements.forEach(element => observer.observe(element));
 });
+
+ // Mostrar el botón cuando se hace scroll hacia abajo
+ window.onscroll = function() {
+  var scrollToTopBtn = document.getElementById("scrollToTopBtn");
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      scrollToTopBtn.style.display = "block";
+  } else {
+      scrollToTopBtn.style.display = "none";
+  }
+};
+
+// Función para subir hasta arriba
+function scrollToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
